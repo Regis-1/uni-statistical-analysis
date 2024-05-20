d5536fc95a7bf2aab669d70e36d413d4fd2f4fb0
Jakub Dudziński (192287) <192287@student.ue.wroc.pl>
2024-05-20 21:37:44 +0200

    data commit #2

--- linear_regression.R
+++ linear_regression.R
@@ -1,30 +1,33 @@
-# reding data with readxl library
+# wczytywanie danych za pomoc¹ biblioteki readxl
 library(readxl)
 dataSet <- read_excel("data/R_data.xlsx")
 
 head(dataSet)
-View(dataSet)
+#View(dataSet)
 
-# linear regression 
-# (Flats_given_for_rental_or_sell ~ Avg_flat_price)
-plot(Avg_flat_price~Flats_given_for_rental_or_sell, data=dataSet)
+# Analiza zmiennych niezale¿nych
+## Dochody bud¿etów woj. ~ zgloszenia uprp
+plot(zgloszenia_uprp~doch_budzet, data=dataSet)
 
-regFPriFGiv <- lm(Avg_flat_price~Flats_given_for_rental_or_sell, data=dataSet)
-summary(regFPriFGiv)
-abline(regFPriFGiv, col='red')
+regBudZglo <- lm(zgloszenia_uprp~doch_budzet, data=dataSet)
+summary(regBudZglo)
+abline(regBudZglo, col='red')
 
-# linear regression
-# (students_count ~ avg_flat_price)
-plot(Avg_flat_price~Students_count, data=dataSet)
+## Iloœæ styp_rektora ~ zgloszenia uprp
+plot(zgloszenia_uprp~styp_rektora, data=dataSet)
 
-regFPriStud <- lm(Avg_flat_price~Students_count, data=dataSet)
-summary(regFPriStud)
-abline(regFPriStud, col='red')
+regStypZglo <- lm(zgloszenia_uprp~styp_rektora, data=dataSet)
+summary(regStypZglo)
+abline(regStypZglo, col='red')
+
+korDochStyp <- cor(
+  dataSet$doch_budzet,
+  dataSet$styp_rektora)
 
 # multiple linear regression
 # (Avg_flat_price ~ Flats_given_for_rental_or_sell + Students_count)
 regMulVar <- lm(
-  Avg_flat_price~Flats_given_for_rental_or_sell+Students_count,
+  zgloszenia_uprp~styp_rektora+doch_budzet,
   data=dataSet)
 
 summary(regMulVar)
